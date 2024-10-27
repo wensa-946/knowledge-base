e86b7c309b799cfd3a9e501371d4d3373104be6c
wensheng <wensheng@whizen.com>
2024-10-27 17:02:12 +0800

    faet: 加入搜索栏、统一明亮/暗黑模式、使用增强语法

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -11,6 +11,25 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav,
     sidebar,
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档"
+          },
+          modal: {
+            noResultsText: "未找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            }
+          }
+        }
+      }
+    },
     socialLinks: [
       { 
         icon: 'github', 
--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -1,11 +1,24 @@
 export default {
-  '/JS阶段/' : [{
-    "text": "JS阶段",
-    items: [
-      { "text": "1.JS基础", link: "/JS阶段/1.JS基础" },
-      { "text": "2.JS进阶", link: "/JS阶段/2.JS进阶" },
-      { "text": "3.ES6", link: "/JS阶段/3.ES6" },
-      { "text": "4.TS", link: "/JS阶段/4.TS" },
-    ]
-  }]
+  '/JS阶段/': [
+    {
+      text: "JS阶段",
+      items: [
+        { "text": "1.JS基础", link: "/JS阶段/1.JS基础" },
+        { "text": "2.JS进阶", link: "/JS阶段/2.JS进阶" },
+        { "text": "3.ES6", link: "/JS阶段/3.ES6" },
+        { "text": "4.TS", link: "/JS阶段/4.TS" },
+      ]
+    },
+    // 为后续拆分 items 内容做准备
+    // {
+    //   text: "2.JS进阶",
+    //   collapsed: true,
+    //   items: [
+    //     { "text": "1.JS基础", link: "/JS阶段/1.JS基础" },
+    //     { "text": "2.JS进阶", link: "/JS阶段/2.JS进阶" },
+    //     { "text": "3.ES6", link: "/JS阶段/3.ES6" },
+    //     { "text": "4.TS", link: "/JS阶段/4.TS" },
+    //   ]
+    // }
+  ]
 }
