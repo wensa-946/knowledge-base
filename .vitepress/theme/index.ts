e484dc49422cd321f35c018d4862c835896bc3ce
wensheng <wensheng@whizen.com>
2024-11-01 21:27:09 +0800

    feat: 使用第三方主题

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,3 +1,9 @@
-import DefaultTheme from 'vitepress/theme'
-import "./custom.css"
-export default DefaultTheme
+// import DefaultTheme from 'vitepress/theme'
+// import "./custom.css"
+// export default DefaultTheme
+// 1. 导入 vitepress 主题
+import Theme from '@escook/vitepress-theme'
+// 2. 导入配套的 CSS 样式（此步骤不能省略）
+import '@escook/vitepress-theme/style.css'
+// 3. 把“导入”的主题“默认导出”即可
+export default Theme
